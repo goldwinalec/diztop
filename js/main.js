865d5d9d5253ffe1d5cede217fb9a3e9d9a8330d
goldwinalec <63246019+goldwinalec@users.noreply.github.com>
2020-11-10 12:04:27 +0500

    Update

--- js/main.js
+++ js/main.js
@@ -1,62 +1,54 @@
-$(document).ready(function(){
-    $('.feedback__items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
     autoplay: true,
-    autoplaySpeed: 10000,
-    arrows: false,
-     responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        centerMode: true,
-        centerPadding: '15px',
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 770,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+    autoplayTimeout: 10000,
+    responsiveClass: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      770: {
+        items: 2,
+      },
+      991: {
+        items: 3,
       }
     }
-  ]
-    });
+  })
 
-    $(".header__nav-toggle").on("click", function () {
+  $(".header__nav-toggle").on("click", function () {
     $("body").toggleClass("no-scroll");
     $(".header__menu").toggleClass("visible");
     $(".header__nav-line").toggleClass("active");
-    });
+  });
 
-    $(".header__menu-link").on("click", function () {
-        $(".header__menu").toggleClass("visible");
-        $(".header__nav-line").toggleClass("active");
-        $("body").removeClass("no-scroll");
-    });
+  $(".header__menu-link").on("click", function () {
+    $(".header__menu").toggleClass("visible");
+    $(".header__nav-line").toggleClass("active");
+    $("body").removeClass("no-scroll");
+  });
+  $(".top-btn").removeClass("visible");
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".top-btn").addClass("visible");
+    } else {
       $(".top-btn").removeClass("visible");
+    }
+  });
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-        $(".top-btn").addClass("visible");
-        } else {
-        $(".top-btn").removeClass("visible");
-        }
-    });
-
-    $(".top-btn").click(function () {
-        $("html, body").animate({ scrollTop: 0 }, 1000);
-    });
-    $('.header__callback').click(function (){
-        $('.modal').toggleClass('open');
-        $('.modal__inner').toggleClass('open');
-    })
+  $(".top-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
+  $('.header__callback').click(function () {
+    $('.modal').toggleClass('open');
+    $('.modal__inner').toggleClass('open');
+  })
 });
 
-function showAlert(){
-    alert('Ваша заявка принята');
+function showAlert() {
+  alert('Ваша заявка принята');
 }
